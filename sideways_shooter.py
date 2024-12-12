0a8bf1a48ac1de7a37602f146a000cb93adba5d5
emiliano <echiper.dev@gmail.com>
2024-12-12 13:32:25 +0200

    Set background color and apply through each loop pass

--- sideways_shooter.py
+++ sideways_shooter.py
@@ -13,6 +13,9 @@ class SidewaysShooter:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Sideways Shooter")
 
+        # Set background color
+        self.bg_color = (31, 40, 45)
+
     def run_game(self):
         """Start the main game loop."""
         while True:
@@ -20,6 +23,9 @@ class SidewaysShooter:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            
+            # Redraw screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
