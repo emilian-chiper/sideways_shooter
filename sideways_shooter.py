fc5eec8b1c590da640e0809ae18637ee6dc17dd5
emiliano <echiper.dev@gmail.com>
2024-12-12 17:53:13 +0200

    Draw ship to screen

--- sideways_shooter.py
+++ sideways_shooter.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class SidewaysShooter:
     """Overall class to manage game assets and behavior."""
@@ -18,6 +19,8 @@ class SidewaysShooter:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Sideways Shooter")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start the main game loop."""
         while True:
@@ -28,6 +31,7 @@ class SidewaysShooter:
             
             # Redraw screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
